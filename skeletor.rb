d1229d64fe70c885dacdc125121c59b0e6c7805f
Jason Nathan <jjnathanjr@gmail.com>
2024-11-03 23:19:33 +0800

    Add skeletor formula

---
+++ skeletor.rb
@@ -0,0 +1,15 @@
+class Skeletor < Formula
+  desc "Your project description here"
+  homepage "https://github.com/jasonnathan/skeletor"
+  url "https://github.com/jasonnathan/skeletor/releases/download/v2.0.1/skeletor.tar.gz" # link to your tar.gz binary
+  sha256 "169bb439a7e69d08967f891cbbaf2e6537ceec9f9eb736ebbb503c9bca83366c"  # Replace this with the actual SHA256 checksum of your tar.gz file
+  license "MIT"
+
+  def install
+    bin.install "skeletor"
+  end
+
+  test do
+    system "#{bin}/skeletor", "--version"
+  end
+end
