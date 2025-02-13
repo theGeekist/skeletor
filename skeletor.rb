346823d2d312a62b1f3ac09916e2ef0e72865d13
Jason Nathan <jjnathanjr@gmail.com>
2025-02-14 03:26:44 +0800

    bumped patch

--- skeletor.rb
+++ skeletor.rb
@@ -1,5 +1,5 @@
 class Skeletor < Formula
-  desc "Your project description here"
+  desc "A super optimised Rust scaffolding tool with snapshot annotations"
   homepage "https://github.com/jasonnathan/skeletor"
   url "https://github.com/jasonnathan/skeletor/releases/download/v2.0.1/skeletor.tar.gz" # link to your tar.gz binary
   sha256 "169bb439a7e69d08967f891cbbaf2e6537ceec9f9eb736ebbb503c9bca83366c"  # Replace this with the actual SHA256 checksum of your tar.gz file
