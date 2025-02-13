9a8bb275af802a9ccf8d103eeb7b22761bf4a8b4
Jason Nathan <gurugeek@geekist.co>
2025-02-14 03:36:25 +0800

    Still fixing workflows

--- skeletor.rb
+++ skeletor.rb
@@ -1,8 +1,8 @@
 class Skeletor < Formula
   desc "A super optimised Rust scaffolding tool with snapshot annotations"
   homepage "https://github.com/jasonnathan/skeletor"
-  url "https://github.com/jasonnathan/skeletor/releases/download/v2.0.1/skeletor.tar.gz" # link to your tar.gz binary
-  sha256 "169bb439a7e69d08967f891cbbaf2e6537ceec9f9eb736ebbb503c9bca83366c"  # Replace this with the actual SHA256 checksum of your tar.gz file
+  url "https://github.com/jasonnathan/skeletor/releases/download/v2.2.7/skeletor-macos-latest-x86_64-apple-darwin.tar.gz" # link to your tar.gz binary.gz" # link to your tar.gz binary
+  sha256 "NEW_SHA256_CHECKSUM_HERE"  # Replace with the actual SHA256 checksum of your tar.gz file
   license "MIT"
 
   def install
