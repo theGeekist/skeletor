0dbd879f3149c8d283c3ec9ff03a7f8ebb78715c
Jason Nathan <gurugeek@geekist.co>
2025-02-14 10:40:48 +0800

    Removed workflow file

--- skeletor.rb
+++ skeletor.rb
@@ -1,7 +1,7 @@
 class Skeletor < Formula
   desc "A super optimised Rust scaffolding tool with snapshot annotations"
   homepage "https://github.com/jasonnathan/skeletor"
-  url "https://github.com/jasonnathan/skeletor/releases/download/v2.2.17/skeletor-macos-x86_64-apple-darwin.tar.gz" # link to your tar.gz binary
+  url "https://github.com/jasonnathan/skeletor/releases/download/v2.2.17/skeletor-macos-x86_64-apple-darwin.tar.gz"
   sha256 "d848937300fecbdc6f0463ab367414e9c976786be15358b973751c0ec7a90c53"
   license "MIT"
 
@@ -10,6 +10,6 @@ class Skeletor < Formula
   end
 
   test do
-    system "#{bin}/skeletor", "--version"
+    system "#{bin}/skeletor", "--help"
   end
 end
